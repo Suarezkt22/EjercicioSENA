b425ca1cfd25698c6c9cf7a501d4a95a4053f28b
Mateo <jssuarez780@gmail.com>
2025-11-26 00:54:08 -0500

    DependencyContainer

--- Server/PruebaGit/DependencyContainer.cs
+++ Server/PruebaGit/DependencyContainer.cs
@@ -1,10 +1,9 @@
 ﻿using Carter;
-using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Options;
 using GitEjercicioSENA.Domain.Contracts;
 using GitEjercicioSENA.Infraestructure.Persistence;
 using GitEjercicioSENA.Infraestructure.Repositories.Dapper;
-
 using GitEjercicioSENA.Infraestructure.Services;
 
 namespace GitEjercicioSENA;
@@ -13,20 +12,24 @@ public static class DependencyContainer
 {
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
+        // ✔ Creamos una conexión SQLite para Dapper
         services.AddScoped(provider =>
         {
             var appSettings = provider.GetRequiredService<IOptions<AppSettings>>().Value;
-            var connection = new SqlConnection(appSettings.DbConnectionString);
+
+            var connection = new SqliteConnection(appSettings.DbConnectionString);
             connection.Open();
             return connection;
         });
 
+        // ✔ DbContext escritura
         services.AddDbContext<DbWriteContext>((serviceProvider, options) =>
         {
             var appSettings = serviceProvider.GetRequiredService<IOptions<AppSettings>>().Value;
             DbContextOptionSetup.ConfigureWriteOptions(options, appSettings.DbConnectionString);
         });
 
+        // ✔ DbContext lectura
         services.AddDbContext<DbReadContext>((serviceProvider, options) =>
         {
             var appSettings = serviceProvider.GetRequiredService<IOptions<AppSettings>>().Value;
@@ -35,11 +38,13 @@ public static class DependencyContainer
 
         services.AddCarter();
 
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+        services.AddMediatR(cfg => 
+            cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
         services.AddScoped<IPasswordService, PasswordService>();
         services.AddScoped<ITokenService, TokenService>();
 
+        // ✔ Repositorios Dapper funcionando con SQLite
         services.AddScoped<IProductRepository, DapperProductRepository>();
         services.AddScoped<IUserRepository, DapperUserRepository>();
         services.AddScoped<IUnitOfWork, DapperUnitOfWork>();
